fddec836368f4f58a39236c45222664943cf0a37
Giant77 <willyarsyad77@gmail.com>
2025-06-04 19:07:20 +0700

    feat: update admin UI

--- barang/admin.py
+++ barang/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Barang
 
-admin.site.register(Barang)
+@admin.register(Barang)
+class ProdukAdmin(admin.ModelAdmin):
+    list_display = ('nama', 'deskripsi', 'jumlah', 'lokasi')
+    search_fields = ('nama','deskripsi')
+    list_filter = ('lokasi',)
+    list_editable = ('jumlah', 'lokasi')
+
 
 '''
     username : admin
